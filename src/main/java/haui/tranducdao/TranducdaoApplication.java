1ea264d1d2e792895d7004815f60e4b2b2d961e1
pogreen <trandaogrey@gmail.com>
2021-02-24 21:55:22 +0700

    base project

---
+++ src/main/java/haui/tranducdao/TranducdaoApplication.java
@@ -0,0 +1,13 @@
+package haui.tranducdao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranducdaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranducdaoApplication.class, args);
+    }
+
+}
